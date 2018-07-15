897bff319ec4437810caaba34f4ee1825957c3dc
ksewen <261517946@qq.com>
2018-07-15 22:01:34 +0800

    create project

---
+++ src/main/java/com/ksewen/refactor/RefactorApplication.java
@@ -0,0 +1,12 @@
+package com.ksewen.refactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorApplication.class, args);
+	}
+}
